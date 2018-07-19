85ee027c95633d280a07480761b5a72e43164880
brayan <brayanolarte972@gmail.com>
2018-07-19 15:12:36 -0500

    Carga inicial del proyecto angular

---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+//import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit {
+
+/*  public retPostData;
+	public retGetData;
+	datosMostrar: any = {};
+	datos: any = {} ;
+
+  constructor(private http: HttpClient) { }
+
+  onNameKeyUp(event: any){
+
+  }*/
+
+  ngOnInit() {
+  //  this.datos.nombre= "";
+  //	this.datosMostrar.FstVarValues= "chao";
+  }
+/*  public imprimir(){
+  	console.log( this.datos );
+  }
+  public PostData() {
+  		const url = "" ;
+  		let datos = {FstVarValues: 'hola', SndVarValues: 'hola mundo'};
+  		this.http.post(url, datos ).subscribe(
+  			data => {
+  				console.log(data);
+  				this.datosMostrar = data;
+  			}
+  		);
+
+  	}
+
+  	public GetData() {
+  		const url = "";
+  		this.http.get(url).subscribe(
+  			data => {
+  				console.log(data);
+  			}
+  		);
+
+  	}
+
+  	public PutData() {
+  		const url = "";
+  		this.http.put(url, {DataVar1:'val1', DataVar2:'val2'})
+  		.subscribe(data => {console.log(data)},err=>{console.log("Error ocurrente")});
+  	}
+
+  	public DeleteData() {
+  		const url = "";
+  		this.http.delete(url).subscribe(
+  			data => {console.log(data)},err=>{console.log("Error ocurrente")});
+  	}
+*/
+}
